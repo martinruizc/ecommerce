ae826d403de24b246317546d633fa5a360c76a10
Martin <martinyoruiz@gmail.com>
2022-11-06 12:17:24 -0500

    User authentication backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler'
 import { User } from '../models/userModel.js'
+import { generateToken } from '../utils/genareToken.js'
 
 
 
@@ -18,11 +19,72 @@ export const authUser = asyncHandler(async (req, res) => {
       last_name: user.last_name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null
+      token: generateToken(user._id)
     })
   } else {
     res.status(401)
     throw new Error('Correo o contraseña incorrecta')
+  }
+
+})
+
+
+// User Profile
+// POST /api/users/profile
+
+export const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+
+})
+
+// User Register user
+// POST /api/users/
+
+export const registerUser = asyncHandler(async (req, res) => {
+  const { first_name, last_name, email, password } = req.body
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(404)
+    throw new Error('Correo en uso')
+  }
+
+  const user = await User.create({
+    first_name,
+    last_name,
+    email,
+    password
+
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id)
+    })
+
+
+  } else {
+    res.status(404)
+    throw new Error('User not found')
   }
 
 })
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,10 @@
 import express from 'express'
-import { authUser } from '../controllers/userController.js'
+import { authUser, getUserProfile, registerUser } from '../controllers/userController.js'
+import { portect } from '../middleware/authMiddleware.js'
 
 export const router = express.Router();
-
+router.route('/').post(registerUser)
 router.post('/login', authUser)
+router.route('/profile').get(portect, getUserProfile)
+
 
